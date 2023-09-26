d002aa35df840722c19272ca68732daa8e0c20b5
Cash Myers <cmyers880@gmail.com>
2023-09-25 23:29:27 -0500

    Initial Commit

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './SignUpForm.css';
+
+export default function SignUpForm() {
+  // const [email, setEmail] = useState('');
+  const [formData, setFormData] = useState({});
+  const [isRegistered, setIsRegistered] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsRegistered(true);
+    // console.log("User signed up with email:", email);
+    // Implement signup logic with the backend
+  };
+
+  return (
+    <div className="dark-mode">
+      {!isRegistered ? (
+        <form id="signinForm" onSubmit={handleSubmit}>
+          <input type="text" name="fullname" placeholder="Full Name" onChange={handleChange} />
+          <input type="email" name="email" placeholder="Email ID" onChange={handleChange} />
+          <input type="email" name="confirmEmail" placeholder="Confirm Email ID" onChange={handleChange} />
+          <input type="tel" name="phone" placeholder="Phone Number" onChange={handleChange} />
+          <label>
+            <input type="checkbox" name="terms" onChange={handleChange} />
+            Agree to our terms and conditions
+          </label>
+          <button type="submit">Register</button>
+        </form>
+      ) : (
+        <p>Thank you for signing up!</p>
+      )}
+    </div>
+  );
+}
